ecede22317e692d9072e0b8caf227481a784bcd2
OMPRAKASH B.K.L <75086005+OmPrakash-bkl@users.noreply.github.com>
2021-07-11 17:28:24 +0530

    My Portfolio

--- index.js
+++ index.js
@@ -13,3 +13,50 @@ window.addEventListener("scroll", function() {
     var scroll = this.document.getElementById('navbar_container');
     scroll.classList.toggle("active", window.scrollY > 450)
 })
+
+function displayimg(value) {
+switch(value) {
+    case 1:
+        document.getElementById("forcol1").style.backgroundColor="rgb(0, 128, 0)";
+        document.getElementById("forcol1").style.boxShadow="0px 0px 40px rgb(194, 194, 194)";
+        document.getElementById("forcol2").style.color="white";
+        document.getElementById("forcol3").style.boxShadow="none";
+        document.getElementById("forcol5").style.boxShadow="none";
+        document.getElementById("forcol3").style.backgroundColor="rgb(238, 238, 238)";
+        document.getElementById("forcol4").style.color="gray";
+        document.getElementById("forcol5").style.backgroundColor="rgb(238, 238, 238)";
+        document.getElementById("forcol6").style.color="gray";
+        document.getElementById("gallery_image_container1").style.display="block";
+        document.getElementById("gallery_image_container2").style.display="none";
+        document.getElementById("gallery_image_container3").style.display="none";
+        break;
+        case 2:
+            document.getElementById("forcol1").style.backgroundColor="rgb(238, 238, 238)";
+            document.getElementById("forcol3").style.boxShadow="0px 0px 40px rgb(194, 194, 194)";
+            document.getElementById("forcol1").style.boxShadow="none";
+            document.getElementById("forcol5").style.boxShadow="none";
+        document.getElementById("forcol2").style.color="gray";
+        document.getElementById("forcol5").style.backgroundColor="rgb(238, 238, 238)";
+        document.getElementById("forcol6").style.color="gray";
+            document.getElementById("forcol3").style.backgroundColor="rgb(0, 128, 0)";
+            document.getElementById("forcol4").style.color="white";
+            document.getElementById("gallery_image_container1").style.display="none";
+            document.getElementById("gallery_image_container2").style.display="block";
+            document.getElementById("gallery_image_container3").style.display="none";
+            break;
+            case 3:
+                document.getElementById("forcol1").style.backgroundColor="rgb(238, 238, 238)";
+                document.getElementById("forcol1").style.boxShadow="none";
+                document.getElementById("forcol3").style.boxShadow="none";
+                document.getElementById("forcol5").style.boxShadow="0px 0px 40px rgb(194, 194, 194)";
+        document.getElementById("forcol2").style.color="gray";
+        document.getElementById("forcol3").style.backgroundColor="rgb(238, 238, 238)";
+        document.getElementById("forcol4").style.color="gray";
+                document.getElementById("forcol5").style.backgroundColor="rgb(0, 128, 0)";
+                document.getElementById("forcol6").style.color="white";
+                document.getElementById("gallery_image_container1").style.display="none";
+                document.getElementById("gallery_image_container2").style.display="none";
+                document.getElementById("gallery_image_container3").style.display="block";
+                break;
+}
+}
